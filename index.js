143806c05256e8ddef1e6c6ff4fba990662ee28e
kanikanidahou <joho.232103@gmail.com>
2024-07-16 09:06:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ function doGet() {
     return HtmlService
         .createTemplateFromFile('index')
         .evaluate()
-        .addMetaTag('viewport', 'width=device-width, initial-scale=1')  //html側にメタタグを設定しても意味がなかったためこちらで設定
+        .addMetaTag('viewport', 'width=device-width, initial-scale=1');  //html側にメタタグを設定しても意味がなかったためこちらで設定
 }
+
 function doPost(e) {
     SendToNotion(e.parameter.title, e.parameter.subtitle, e.parameter.authors, e.parameter.publishedDate,
         Number(e.parameter.pageCount), e.parameter.isbn, e.parameter.imageSrc);
@@ -15,10 +16,9 @@ function SendToNotion(title, subtitle, authors, publishedDate, pageCount, isbn, 
     const notion_key = 'secret_lruJvyFNd3aRm8HD5qmZX5JfKeqQdYaGCTxb5lP1z4E';
     const database_id = '2517262d6cc0429797696292c1e83583';
 
-    json_data = {
+    let json_data = {
         'parent': { 'database_id': database_id },
         'properties': {
-            //プロパティを記述する
             'Title': {
                 'title': [
                     {
@@ -64,14 +64,13 @@ function SendToNotion(title, subtitle, authors, publishedDate, pageCount, isbn, 
             'PageCount': {
                 'number': pageCount
             },
-            'Cover': {   //本文からプロパティに移行
-                "type": "files",
-                "files": [
+            'Cover': {
+                'files': [
                     {
-                        "name": "Cover",
-                        "type": "external",
-                        "external": {
-                            "url": imageSrc
+                        'name': 'Cover',
+                        'type': 'external',
+                        'external': {
+                            'url': imageSrc
                         }
                     }
                 ]
@@ -86,19 +85,18 @@ function SendToNotion(title, subtitle, authors, publishedDate, pageCount, isbn, 
                 ]
             }
         }
-        //ここから本文だが、今回は使わない
-        // 'children': [
-        // ]
     };
 
-    const response = UrlFetchApp.fetch('https://www.notion.so/2517262d6cc0429797696292c1e83583?pvs=4', {
-        "method": "post",
-        "headers": {
+    const response = UrlFetchApp.fetch('https://api.notion.com/v1/pages', {
+        'method': 'post',
+        'headers': {
             'Content-Type': 'application/json; charset=UTF-8',
             'Authorization': 'Bearer ' + notion_key,
             'Notion-Version': '2021-05-13',
         },
-        "payload": JSON.stringify(json_data),
+        'payload': JSON.stringify(json_data),
         'muteHttpExceptions': true
     });
+
+    
 }
